d4140e0bc29fedced961a341c1e0b19a4c28325f
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-06 14:22:01 +0100

    setting up app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    initialRoute: '/home', // Change the default home route
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
   ));
 }
