a5d058ce318e7429d4e0d6ce7ab593ca4da8cd08
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-06 12:41:49 +0100

    fixing home import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/pages/home.';
+import 'package:world_time/pages/home.dart';
 void main() {
   runApp(MaterialApp(
     home: Home()
