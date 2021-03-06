a1bbe0e943fcab5600e70a7dae631c1159d6a8c4
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-06 12:40:42 +0100

    init world time app

---
+++ android/app/src/main/java/com/mouhsine/world_time/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mouhsine.world_time;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/home.';
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+}
