e401dd8c801887c3fe7c2f115663a6f0b4f1891e
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-07 03:52:31 +0100

    fetching date time from woldtime server and adding offset

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,16 +10,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async{
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async{
+
+    // Make the request
+    Response response = await get('https://worldtimeapi.org/api/timezone/Africa/Casablanca');
     Map data = jsonDecode(response.body);
-    print(data['title']);
+    //  print(data);
+
+    //  Get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(dateTime);
+    // print(offset);
+
+    //  Create a dateTime object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
