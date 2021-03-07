0d5fcdd159366627ab2773d99426085269355462
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-07 16:28:16 +0100

    Setting up world time service

---
+++ lib/services/world_time.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; //  location name for the UI
+  String time; // Time in that location
+  String flag;  //  url to the asset flag icon
+  String url; //  location url for the api endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+  void getTime() async {
+
+    //  Make the request
+    Response response = await get("http://worldtimeapi.org/api/timezone/$url");
+    Map data = jsonDecode(response.body);
+
+    //  Get properties from data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    //  Create DateTime object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    //  Set the time property
+    time = now.toString();
+  }
+}
