2b66de0b18da45bacd314c63b26e1f7d5bc542ab
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-07 17:24:40 +0100

    passing time, location and flag to route from loading widget to home widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
     return Scaffold(
       body: SafeArea(
         child: Column(
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,7 +13,11 @@ class _LoadingState extends State<Loading> {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+    });
   }
 
   @override
