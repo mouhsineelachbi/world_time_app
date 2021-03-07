742600ba76c3222346ec8f0a26d4fd20435599e2
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-03-07 23:11:44 +0100

    setting up the backgroud for daytime and nighttime and color for location icon and label and time color

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,41 +15,64 @@ class _HomeState extends State<Home> {
     // Grabbing data from route arguments send by loading widget
     data = ModalRoute.of(context).settings.arguments;
     print(data);
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0 ,0),
-          child: Column(
-            children: [
-              FlatButton.icon(
-                  onPressed: (){
-                    Navigator.pushNamed(context, '/location');
-                  },
-                  icon: Icon(Icons.edit_location),
-                  label: Text('Edit location'),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    )
-                  )
-                ],
-              ),
-              SizedBox(height: 28.0,),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
 
+    //  Set background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0 ,0),
+            child: Column(
+              children: [
+                FlatButton.icon(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
+                    ),
+                    label: Text(
+                      'Edit location',
+                      style: TextStyle(
+                        color: Colors.grey[300],
+                      ),
+                    ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      )
+                    )
+                  ],
+                ),
+                SizedBox(height: 28.0,),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,6 +8,7 @@ class WorldTime {
   String time; // Time in that location
   String flag;  //  url to the asset flag icon
   String url; //  location url for the api endpoint
+  bool isDaytime; // true of false if It's daytime or not
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -25,7 +26,8 @@ class WorldTime {
       //  Create DateTime object
       DateTime now = DateTime.parse(dateTime);
       now = now.add(Duration(hours: int.parse(offset)));
-
+      // verify if It's daytime or not
+      isDaytime = now.hour > 6 && now.hour < 18 ? true : false;
       //  Set the time property
       time = DateFormat.jm().format(now);
 
